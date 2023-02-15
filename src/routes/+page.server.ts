20e12e6970b04d3b9cf43cbea745e22fb0e83ff4
Johan Lindgren <johan@lindgr3n.com>
2023-02-15 20:46:44 +0100

    fix: remove custom mail for now until dns is fixed

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import { fail } from '@sveltejs/kit';
-import { sendMail } from '../lib/server/mail';
+import { addToMailList } from '../lib/server/mail';
 import type { PageServerLoad, Actions } from './$types';
 
 export const load: PageServerLoad = async function () {
@@ -15,12 +15,25 @@ export const actions: Actions = {
 			return fail(403, { email: 'missing' });
 		}
 
-		await sendMail({
-			from: 'hackathon@ovik.dev',
-			to: email,
-			subject: 'Ovik.dev - Hackathon',
-			html: `<p>Thanks for showing interest in Ovik.dev Hackathon</p><br/><br/>Not done yet :)`
-		});
+		// TODO: Need to support custom domain before we can send a custom message
+		// await sendMail({
+		// 	from: 'hackathon@ovik.dev',
+		// 	to: email,
+		// 	subject: 'Ovik.dev - Hackathon',
+		// 	html: `<p>Thanks for showing interest in Ovik.dev Hackathon</p><br/><br/>Not done yet :)`
+		// });
+
+		// Add the user to the maillist
+		await addToMailList('ovik_dev_hackathon', [
+			{
+				name: '',
+				address: email,
+				subscribed: true,
+				vars: {
+					user_id: ''
+				}
+			}
+		]);
 
 		return {};
 	}
