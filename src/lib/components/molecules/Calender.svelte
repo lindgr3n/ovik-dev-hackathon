0dcdad4e7782bcff5522ae56a15630911db6ae8c
Johan Lindgren <johan@lindgr3n.com>
2023-02-15 21:55:19 +0100

    feat: basic calender

---
+++ src/lib/components/molecules/Calender.svelte
@@ -0,0 +1,207 @@
+<script lang="ts"></script>
+
+<div class="isolate flex flex-auto overflow-hidden bg-white">
+	<div class="flex flex-auto flex-col overflow-auto">
+		<div class="flex w-full flex-auto">
+			<div class="w-14 flex-none bg-white ring-1 ring-gray-100" />
+			<div class="grid flex-auto grid-cols-1 grid-rows-1">
+				<!-- Horizontal lines -->
+				<div
+					class="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100"
+					style="grid-template-rows: repeat(48, minmax(3.5rem, 1fr))"
+				>
+					<div class="row-end-1 h-7" />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							00:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							01:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							02:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							03:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							04:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							05:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							06:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							07:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							08:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							09:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							10:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							11:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							12:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							13:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							14:00PM
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							15:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							16:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							17:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							18:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							19:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							20:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							21:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							22:00
+						</div>
+					</div>
+					<div />
+					<div>
+						<div class="-mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+							23:00
+						</div>
+					</div>
+					<div />
+				</div>
+
+				<!-- Events -->
+				<!-- 6 span for every hour -->
+				<ol
+					class="col-start-1 col-end-2 row-start-1 grid grid-cols-1"
+					style="grid-template-rows: 1.75rem repeat(288, minmax(0, 1fr)) auto"
+				>
+					<!-- <li class="relative mt-px flex" style="grid-row: 6 / span 6">
+						<a
+							href="#"
+							class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-blue-50 p-2 text-xs leading-5 hover:bg-blue-100"
+						>
+							<p class="order-1 font-semibold text-blue-700">Breakfast</p>
+							<p class="text-blue-500 group-hover:text-blue-700">
+								<time datetime="2022-01-22T06:00">6:00 AM</time>
+							</p>
+						</a>
+					</li>
+					<li class="relative mt-px flex" style="grid-row: 92 / span 30">
+						<a
+							href="#"
+							class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-pink-50 p-2 text-xs leading-5 hover:bg-pink-100"
+						>
+							<p class="order-1 font-semibold text-pink-700">Flight to Paris</p>
+							<p class="order-1 text-pink-500 group-hover:text-pink-700">
+								John F. Kennedy International Airport
+							</p>
+							<p class="text-pink-500 group-hover:text-pink-700">
+								<time datetime="2022-01-22T07:30">7:30 AM</time>
+							</p>
+						</a>
+					</li>
+					<li class="relative mt-px flex" style="grid-row: 134 / span 18">
+						<a
+							href="#"
+							class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-indigo-50 p-2 text-xs leading-5 hover:bg-indigo-100"
+						>
+							<p class="order-1 font-semibold text-indigo-700">Sightseeing</p>
+							<p class="order-1 text-indigo-500 group-hover:text-indigo-700">Eiffel Tower</p>
+							<p class="text-indigo-500 group-hover:text-indigo-700">
+								<time datetime="2022-01-22T11:00">11:00 AM</time>
+							</p>
+						</a>
+					</li> -->
+				</ol>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,3 +1,89 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import Calender from '$lib/components/molecules/Calender.svelte';
 
-TODO: fix me
+	let selected: 'friday' | 'saturday' | 'sunday' = 'friday';
+</script>
+
+<div class="flex h-full flex-col">
+	<div class="pt-8">
+		<div class="sm:hidden">
+			<label for="tabs" class="sr-only">Select a tab</label>
+			<!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
+			<select
+				id="tabs"
+				name="tabs"
+				class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+				bind:value={selected}
+			>
+				<option value="friday">Friday - May ??</option>
+				<option value="saturday">Saturday - May ??</option>
+				<option value="sunday">Sunday - May ??</option>
+			</select>
+		</div>
+		<div class="hidden sm:block">
+			<div class="border-b border-gray-200">
+				<nav class="-mb-px flex justify-center" aria-label="Tabs">
+					<!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" -->
+					<a
+						href="#"
+						class={`${
+							selected == 'friday'
+								? 'border-indigo-500 text-indigo-600'
+								: 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+						} w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm`}
+						on:click={() => (selected = 'friday')}
+						><div>
+							<h1 class="text-lg font-semibold leading-6 text-gray-900">Friday</h1>
+							<time datetime="2023-05-01" class="sm:hidden mt-1 text-sm text-gray-500"
+								>May ??, 2023</time
+							>
+							<time datetime="2023-05-01" class="hidden sm:inline mt-1 text-sm text-gray-500"
+								>May ??, 2023</time
+							>
+						</div></a
+					>
+
+					<a
+						href="#"
+						class={`${
+							selected == 'saturday'
+								? 'border-indigo-500 text-indigo-600'
+								: 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+						} w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm`}
+						on:click={() => (selected = 'saturday')}
+						><div>
+							<h1 class="text-lg font-semibold leading-6 text-gray-900">Saturday</h1>
+							<time datetime="2023-05-01" class="sm:hidden mt-1 text-sm text-gray-500"
+								>May ??, 2023</time
+							>
+							<time datetime="2023-05-01" class="hidden sm:inline mt-1 text-sm text-gray-500"
+								>May ??, 2023</time
+							>
+						</div></a
+					>
+
+					<a
+						href="#"
+						class={`${
+							selected == 'sunday'
+								? 'border-indigo-500 text-indigo-600'
+								: 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+						} w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm`}
+						on:click={() => (selected = 'sunday')}
+						><div>
+							<h1 class="text-lg font-semibold leading-6 text-gray-900">Sunday</h1>
+							<time datetime="2023-05-23" class="sm:hidden mt-1 text-sm text-gray-500"
+								>May ??, 2023</time
+							>
+							<time datetime="2023-05-23" class="hidden sm:inline mt-1 text-sm text-gray-500"
+								>May ??, 2023</time
+							>
+						</div></a
+					>
+				</nav>
+			</div>
+		</div>
+	</div>
+
+	<Calender />
+</div>
