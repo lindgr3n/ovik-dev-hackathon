d662321c256e3b7dd8beed685ad4a6ff29b7d601
Johan Lindgren <johan@lindgr3n.com>
2023-02-15 20:20:07 +0100

    feat: basic profile page still in wip

---
+++ src/routes/profile/+page.server.ts
@@ -0,0 +1,42 @@
+import { error, fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
+
+export const load: PageServerLoad = async function ({ locals }) {
+	// Check if we have a user otherwise redirect to start
+	const user = locals.user;
+	if (!user) {
+		throw redirect(302, '/');
+	}
+	const roles = [
+		{
+			value: 'DEVELOPER',
+			name: 'Developer'
+		},
+		{
+			value: 'DESIGNER',
+			name: 'Designer'
+		},
+		{
+			value: 'PROJECT_MANAGER',
+			name: 'Project manager'
+		},
+		{
+			value: 'TESTER',
+			name: 'Test'
+		},
+		{
+			value: 'OTHER',
+			name: 'Other'
+		}
+	];
+	return {
+		roles: roles
+	};
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		// TODO: Update the user information
+		return {};
+	}
+};
