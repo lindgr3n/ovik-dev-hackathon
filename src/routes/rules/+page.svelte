fc5725e45e17da81c8e5e5bd172a3b608e9308d1
Johan Lindgren <johan@lindgr3n.com>
2023-02-20 23:39:44 +0100

    feat: rules page

--- src/routes/rules/+page.svelte
+++ src/routes/rules/+page.svelte
@@ -1,3 +1,167 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import HighlightText from '$lib/components/molecules/HighlightText.svelte';
+</script>
 
-Be nice 😀
+<div class="bg-white py-24 sm:py-32">
+	<div class="mx-auto max-w-7xl px-6 lg:px-8">
+		<div
+			class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+		>
+			<div>
+				<h2 class="text-lg font-semibold text-cyan-600">By attending you agree to the following</h2>
+				<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Event rules</p>
+				<p class="mt-2 text-base leading-7 text-gray-600">
+					We want a open and safe enviroment for <HighlightText>everyone</HighlightText>. If you
+					don't feel that way reach out to one of the organizers
+				</p>
+			</div>
+			<dl
+				class="col-span-2 grid grid-cols-1 gap-x-8 gap-y-6 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-10"
+			>
+				<div class="relative pl-9">
+					<dt class="font-semibold text-gray-900">
+						<svg
+							class="absolute top-1 left-0 h-5 w-5 text-indigo-500"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Provide a friendly enviroment
+					</dt>
+				</div>
+
+				<div class="relative pl-9">
+					<dt class="font-semibold text-gray-900">
+						<svg
+							class="absolute top-1 left-0 h-5 w-5 text-indigo-500"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Zero tolerance for bullying
+					</dt>
+				</div>
+
+				<div class="relative pl-9">
+					<dt class="font-semibold text-gray-900">
+						<svg
+							class="absolute top-1 left-0 h-5 w-5 text-indigo-500"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Being helpful to others
+					</dt>
+				</div>
+
+				<div class="relative pl-9">
+					<dt class="font-semibold text-gray-900">
+						<svg
+							class="absolute top-1 left-0 h-5 w-5 text-indigo-500"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Zero tolerance for harassment
+					</dt>
+				</div>
+
+				<div class="relative pl-9">
+					<dt class="font-semibold text-gray-900">
+						<svg
+							class="absolute top-1 left-0 h-5 w-5 text-indigo-500"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Being a team member
+					</dt>
+				</div>
+
+				<div class="relative pl-9">
+					<dt class="font-semibold text-gray-900">
+						<svg
+							class="absolute top-1 left-0 h-5 w-5 text-indigo-500"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Zero tolerance for violence
+					</dt>
+				</div>
+
+				<div class="relative pl-9">
+					<dt class="font-semibold text-gray-900">
+						<svg
+							class="absolute top-1 left-0 h-5 w-5 text-indigo-500"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Having fun 😀 🙌
+					</dt>
+				</div>
+
+				<!-- <div class="relative pl-9">
+					<dt class="font-semibold text-gray-900">
+						<svg
+							class="absolute top-1 left-0 h-5 w-5 text-indigo-500"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						
+					</dt>
+				</div> -->
+			</dl>
+		</div>
+	</div>
+</div>
